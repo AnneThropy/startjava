a24768d583d24824936bca5abe619ca8c4ede468
Anna Veretennyk <Agape@Annas-MacBook-Pro.local>
2020-03-01 17:12:33 +0700

    implement HW1

---
+++ Lesson1/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+	public static void main(String[] args) {
+		boolean pc = true;
+		byte ram = 8;
+		short ssd = 512;
+		int year = 2015;
+		long graphics = 61001536;
+		float cpu = 2.9F;
+		double os = 10.136;
+		char web = 9784;
+		System.out.println(web+"  Hi, do you have PC?  "+pc+"!  What are the specs? Ram:"+ram+",  SSD:"+ssd+",  CPU:"+cpu+",  Graphics:"+graphics+",  OS:"+os+",  Year:"+year+"  "+web);
+	}
+
+}
